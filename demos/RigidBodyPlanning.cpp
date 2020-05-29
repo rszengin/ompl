25c47a66b242211ee636ef22f37986ca9880811c
Rahman Salim Zengin <rszengin@itu.edu.tr>
2020-05-29 17:24:20 +0000

    RRTStar ve DubinsStateSpace ile SimpleSetup

--- demos/RigidBodyPlanning.cpp
+++ demos/RigidBodyPlanning.cpp
@@ -35,8 +35,8 @@
 /* Author: Ioan Sucan */
 
 #include <ompl/base/SpaceInformation.h>
-#include <ompl/base/spaces/SE2StateSpace.h>
-#include <ompl/geometric/planners/rrt/RRTConnect.h>
+#include <ompl/base/spaces/DubinsStateSpace.h>
+#include <ompl/geometric/planners/rrt/RRTstar.h>
 #include <ompl/geometric/SimpleSetup.h>
 
 #include <ompl/config.h>
@@ -48,13 +48,13 @@ namespace og = ompl::geometric;
 bool isStateValid(const ob::State *state)
 {
     // cast the abstract state type to the type we expect
-    const auto *se2state = state->as<ob::SE2StateSpace::StateType>();
+    const auto *dubins_state = state->as<ob::DubinsStateSpace::StateType>();
 
     // extract the first component of the state and cast it to what we expect
-    const auto *pos = se2state->as<ob::RealVectorStateSpace::StateType>(0);
+    const auto *pos = dubins_state->as<ob::RealVectorStateSpace::StateType>(0);
 
     // extract the second component of the state and cast it to what we expect
-    const auto *rot = se2state->as<ob::SO2StateSpace::StateType>(1);
+    const auto *rot = dubins_state->as<ob::SO2StateSpace::StateType>(1);
 
     // check validity of state defined by pos & rot
 
@@ -63,75 +63,10 @@ bool isStateValid(const ob::State *state)
     return (const void*)rot != (const void*)pos;
 }
 
-void plan()
-{
-    // construct the state space we are planning in
-    auto space(std::make_shared<ob::SE2StateSpace>());
-
-    // set the bounds for the R^2 part of SE(2)
-    ob::RealVectorBounds bounds(2);
-    bounds.setLow(-1);
-    bounds.setHigh(1);
-
-    space->setBounds(bounds);
-
-    // construct an instance of  space information from this state space
-    auto si(std::make_shared<ob::SpaceInformation>(space));
-
-    // set state validity checking for this space
-    si->setStateValidityChecker(isStateValid);
-
-    // create a random start state
-    ob::ScopedState<> start(space);
-    start.random();
-
-    // create a random goal state
-    ob::ScopedState<> goal(space);
-    goal.random();
-
-    // create a problem instance
-    auto pdef(std::make_shared<ob::ProblemDefinition>(si));
-
-    // set the start and goal states
-    pdef->setStartAndGoalStates(start, goal);
-
-    // create a planner for the defined space
-    auto planner(std::make_shared<og::RRTConnect>(si));
-
-    // set the problem we are trying to solve for the planner
-    planner->setProblemDefinition(pdef);
-
-    // perform setup steps for the planner
-    planner->setup();
-
-
-    // print the settings for this space
-    si->printSettings(std::cout);
-
-    // print the problem settings
-    pdef->print(std::cout);
-
-    // attempt to solve the problem within one second of planning time
-    ob::PlannerStatus solved = planner->ob::Planner::solve(1.0);
-
-    if (solved)
-    {
-        // get the goal representation from the problem definition (not the same as the goal state)
-        // and inquire about the found path
-        ob::PathPtr path = pdef->getSolutionPath();
-        std::cout << "Found solution:" << std::endl;
-
-        // print the path to screen
-        path->print(std::cout);
-    }
-    else
-        std::cout << "No solution found" << std::endl;
-}
-
 void planWithSimpleSetup()
 {
     // construct the state space we are planning in
-    auto space(std::make_shared<ob::SE2StateSpace>());
+    auto space(std::make_shared<ob::DubinsStateSpace>(1.0, false));
 
     // set the bounds for the R^2 part of SE(2)
     ob::RealVectorBounds bounds(2);
@@ -143,7 +78,7 @@ void planWithSimpleSetup()
     // define a simple setup class
     og::SimpleSetup ss(space);
     
-    ss.setPlanner(std::make_shared<og::RRTConnect>(ss.getSpaceInformation()));
+    ss.setPlanner(std::make_shared<og::RRTstar>(ss.getSpaceInformation()));
 
     // set state validity checking for this space
     ss.setStateValidityChecker([](const ob::State *state) { return isStateValid(state); });
@@ -180,10 +115,6 @@ void planWithSimpleSetup()
 int main(int /*argc*/, char ** /*argv*/)
 {
     std::cout << "OMPL version: " << OMPL_VERSION << std::endl;
-
-    // plan();
-
-    std::cout << std::endl << std::endl;
 
     planWithSimpleSetup();
 
